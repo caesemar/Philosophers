8f2864944faf70d8303c2519a6a5b71ff1247b18
caesemar <caesemar@gmail.com>
2023-11-12 00:12:33 +0100

    xd

--- header/Philosophers.h
+++ header/Philosophers.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Philosophers.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jocasado <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jocasado <jocasado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 15:19:15 by jocasado          #+#    #+#             */
-/*   Updated: 2023/07/28 15:19:15 by jocasado         ###   ########.fr       */
+/*   Updated: 2023/11/11 22:48:08 by jocasado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,28 @@
 # include <sys/time.h>
 # include <limits.h>
 
-typedef struct s_philosopher
+typedef struct table
+{
+
+}
+typedef struct s_init
 {
 	int	number_of_philosophers;
 	int	time_to_die;
 	int	time_to_eat;
 	int	time_to_sleep;
 	int	number_of_times_each_philosopher_must_eat;
-}	t_philosopher;
+}	t_init;
+
+typedef struct s_philo
+{
+	pthread_t	*philosopher;
+	int	time_until_death;
+	int	time_eating;
+	int	time_sleeping;
+	int	time_til_full;
+	int fork;
+}	t_philo;
 
 size_t	ft_strlen(const char *s);
 int		ft_atoi(const char *str);
@@ -39,6 +53,6 @@ char	*ft_itoa(int n);
 void	is_positive(char **argv, int argc);
 int		ft_atoi(const char *str);
 void	ft_error2(void);
-void	print_philo(t_philosopher *philo);
+void	print_philo(s_init *philo);
 
 #endif
--- source/Philosophers.c
+++ source/Philosophers.c
@@ -1,27 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Philosophers.c                                     :+:      :+:    :+:   */
+/*   initsophers.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jocasado <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jocasado <jocasado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 15:18:27 by jocasado          #+#    #+#             */
-/*   Updated: 2023/07/28 15:18:27 by jocasado         ###   ########.fr       */
+/*   Updated: 2023/11/11 21:03:26 by jocasado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../header/Philosophers.h"
+#include "../header/philosophers.h"
 
 int	main(int argc, char **argv)
 {
-	t_philosopher	philo;
+	t_init	init;
 
-	parse_args(&philo, argc, argv);
-	print_philo(&philo);
+	parse_args(&init, argc, argv);
+	print_init(&init);
 	return (0);
 }
 
-void	parse_args(t_philosopher *philo, int argc, char **argv)
+void	parse_args(t_init *init, int argc, char **argv)
 {
 	int	i;
 
@@ -38,12 +38,16 @@ void	parse_args(t_philosopher *philo, int argc, char **argv)
 			ft_error2();
 	}
 	is_positive(argv, argc);
-	philo->number_of_philosophers = ft_atoi(argv[1]);
-	philo->time_to_die = ft_atoi(argv[2]);
-	philo->time_to_eat = ft_atoi(argv[3]);
-	philo->time_to_sleep = ft_atoi(argv[4]);
+	init->number_of_philosophers = ft_atoi(argv[1]);
+	init->time_to_die = ft_atoi(argv[2]);
+	init->time_to_eat = ft_atoi(argv[3]);
+	init->time_to_sleep = ft_atoi(argv[4]);
 	if (argc == 6)
-		philo->number_of_times_each_philosopher_must_eat = ft_atoi(argv[5]);
+		init->number_of_times_each_philosopher_must_eat = ft_atoi(argv[5]);
 	else
-		philo->number_of_times_each_philosopher_must_eat = -1;
+		init->number_of_times_each_philosopher_must_eat = -1;
+}
+void	init(t_philo *philo)
+{
+
 }
