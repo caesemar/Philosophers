27c5969f60c627075b288a5c8ee7aed5c028b410
root <root@Stitch>
2024-02-28 00:49:40 +0100

    norminette

--- source/things.c
+++ source/things.c
@@ -6,7 +6,7 @@
 /*   By: jocasado <jocasado@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/25 19:49:47 by jocasado          #+#    #+#             */
-/*   Updated: 2024/02/25 22:24:04 by jocasado         ###   ########.fr       */
+/*   Updated: 2024/02/28 00:49:02 by jocasado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 void	wait_threads(t_philo *ph, int nbr_philos)
 {
-    int i;
+	int	i;
 
-    i = -1;
+	i = -1;
 	while (++i < nbr_philos)
 		pthread_join(ph[i].thread, NULL);
-    i = -1;
-    while (++i < nbr_philos)
-        pthread_mutex_destroy(&ph[i].left_fork);
-    pthread_mutex_destroy(&ph[0].values->m_print);
-    pthread_mutex_destroy(&ph[0].values->m_dead);
-    pthread_mutex_destroy(&ph[0].values->meal_lock);
+	i = -1;
+	while (++i < nbr_philos)
+		pthread_mutex_destroy(&ph[i].left_fork);
+	pthread_mutex_destroy(&ph[0].values->m_print);
+	pthread_mutex_destroy(&ph[0].values->m_dead);
+	pthread_mutex_destroy(&ph[0].values->meal_lock);
 	free(ph);
 }
